21db7241af54d70f162fbce1876c461d04b0cc5b
Tolibjon <tolibjohn@gmail.com>
2021-06-11 17:00:39 +0500

    check for API data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+
   const quote='https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
   const xhr=new XMLHttpRequest();
   xhr.open('GET', quote);
 
   xhr.onload = ()=>{
    const data=JSON.parse(xhr.responseText);
-   console.log(data);
+   console.log(data.quotes);
 };
 
 xhr.send();
