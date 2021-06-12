3f0fa4b8c6357563cf2be67f124155808c5d1ecc
Tolibjon <tolibjohn@gmail.com>
2021-06-12 16:42:08 +0500

    Add functionality for changing quotes and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 import Board from './Components/Board';
 
 function App() {
+  const [quote, setQuote]=useState([]);
+  const [quoteReady, setQuoteReady]=useState(false);
+  const quotes='https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+ 
 
-  const quote='https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+useEffect(()=>{
   const xhr=new XMLHttpRequest();
-  xhr.open('GET', quote);
+  xhr.open('GET', quotes);
 
   xhr.onload = ()=>{
    const data=JSON.parse(xhr.responseText);
-   console.log(data.quotes);
+   setQuote(data.quotes);
+   setQuoteReady(true)
 };
-
 xhr.send();
-  
-  
-  
+},[])
+
+
+
+  const [color, setColor]=useState('purple');
+  const colors=['red', 'orange', 'yellow', 'green', 'blue', 'pink', 'purple', 'brown', 'black'];
+
+  const changeColor=()=>{
+    const rndColor = Math.round(Math.random()*8);
+
+    setColor(colors[rndColor]);
+  }
+
+
   return (
-    <div className="App" id="quote-box">
-      <Board />
+    <div className="App" id="quote-box" style={{backgroundColor:color}}>
+      {quoteReady?<Board changeColor={changeColor} color={color} quoteList={quote}/>:null}
+      <div style={{color:'#fff', padding:'2em'}}>
+        created by <a href='https://github.com/toliboff' target="_blank" rel="noreferrer" style={{textDecoration:'none', color:'#ccc'}}>toliboff</a>
+      </div>
+      
     </div>
   );
 }
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,23 +1,76 @@
 import React from 'react';
 import {useState} from 'react';
 
-export default function Board() {
+export default function Board(props) {
+   const [quote, setQuote] = useState(props.quoteList[0]);
 
-  const [quote, setQuote] = useState({text:'When I was 5 years old, my mother always told me that happiness was the key to life. When I went to school, they asked me what I wanted to be when I grew up. I wrote down ‘happy’. They told me I didn’t understand the assignment, and I told them they didn’t understand life.',
-                                      author:'John Lennon'});
-                                      
+  const styles={
+    box:{
+      color:props.color,
+      backgroundColor:'#fff', 
+      transition:'2s',
+      width:'50%',
+      padding:'3em',
+      borderRadius:'15px',
+      fontSize:'1.5em'
+    },
+    quote:{
+      // color:'#fff'
+    },
+    socialMedia:{
+      display:'flex',
+      width:'85%',
+      margin:'20px auto',
+      fontSize: '0.8em'
+    },
+    social:{
+      backgroundColor: props.color,
+      color:'#fff',
+      cursor: 'pointer',
+      transition: '2s',
+      width:'25px',
+      padding:'0.5em 1em',
+      margin:'0',
+      borderRadius:'3px',
+      marginRight:'10px',
+      display:'flex',
+      justifyContent:'center',
+    },
+    button:{
+       backgroundColor: props.color,
+       color:'#fff',
+       cursor: 'pointer',
+       transition: '2s',
+       padding:'0.5em 1em',
+       marginLeft: 'auto',
+       borderRadius:'3px',
+    },
+    fadeOut:{
+      // opacity:'100%',
+      // transition:'1s'
+    }
+   
+  }    
+  const pickQuote=()=>{
+    document.getElementById('quote').classList.add('fadeOut');
+    setQuote(props.quoteList[Math.round(Math.random()*102)]);
+
+  }     
+
   return (
-    <div id='quote-box'>
-      <p>
-        {quote.text}
+    <div id='quote-box' style={styles.box}>
+      <p style={styles.quote} id='quote'>
+        {quote.quote}
       </p>
       <span>{quote.author}</span>
 
-      <div>
-        <div className='social-media'>
-          <i class="fab fa-twitter-square"></i>
-          <i class="fab fa-tumblr-square"></i>
-        </div>
+      <div style={styles.socialMedia}> 
+        
+        <span style={styles.social}><i className="fab fa-twitter"></i></span>
+        <span style={styles.social}><i className="fab fa-tumblr"></i></span>
+           
+        <span onClick={()=>{props.changeColor(); pickQuote()}} style={styles.button}>New Quote</span>
+      
       </div>
     </div>
   )
